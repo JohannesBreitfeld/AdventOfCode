538946ab8cbf61ca18f0b7a02dc15baaa8e86e27
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-12-01 16:32:45 +0100

    Add project files.

---
+++ AOC2401/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+var path = Path.Combine("..", "..", "..", "..", "Input01.txt");
+String[] input = File.ReadAllLines(path);
+
+var left = input.Select(line => int.Parse(line.Substring(0, 5))).ToList();
+var right = input.Select(line => int.Parse(line.Substring(8, 5))).ToList();
+
+//long total = Problem1(left, right);
+long total = Problem2(left, right);
+
+Console.WriteLine(total);
+
+static long Problem1(List<int> left, List<int> right)
+{
+    var leftOrdered = left.OrderBy(value => value).ToList();
+    var rightOrdered = right.OrderBy(value => value).ToList();
+
+    long total = leftOrdered.Zip(rightOrdered, (l, r) => Math.Abs(l - r)).Sum();
+    return total;
+}
+
+static long Problem2(List<int> left, List<int> right)
+{
+    long total = left.Sum(l => l * right.Count(r => r == l));
+
+    return total;
+}
