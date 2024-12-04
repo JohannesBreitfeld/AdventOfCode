e1c71d0a37860979860e1a64596651c01bc2f099
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-12-04 22:07:08 +0100

    ADD new project for day 04

--- AOC2401/Program.cs
+++ AOC2401/Program.cs
@@ -3,24 +3,24 @@
 var path = Path.Combine("..", "..", "..", "..","inputs", "Input01.txt");
 String[] input = File.ReadAllLines(path);
 
-var left = input.Select(line => int.Parse(line.Substring(0, 5))).ToList();
-var right = input.Select(line => int.Parse(line.Substring(8, 5))).ToList();
+var left = input.Select(line => int.Parse(line.Substring(0, 5)));
+var right = input.Select(line => int.Parse(line.Substring(8, 5)));
 
 //long total = Problem1(left, right);
 long total = Problem2(left, right);
 
 Console.WriteLine(total);
 
-static long Problem1(List<int> left, List<int> right)
+static long Problem1(IEnumerable<int> left, IEnumerable<int> right)
 {
-    var leftOrdered = left.OrderBy(value => value).ToList();
-    var rightOrdered = right.OrderBy(value => value).ToList();
+    var leftOrdered = left.OrderBy(value => value);
+    var rightOrdered = right.OrderBy(value => value);
 
     long total = leftOrdered.Zip(rightOrdered, (l, r) => Math.Abs(l - r)).Sum();
     return total;
 }
 
-static long Problem2(List<int> left, List<int> right)
+static long Problem2(IEnumerable<int> left, IEnumerable<int> right)
 {
     long total = left.Sum(l => l * right.Count(r => r == l));
 
---
+++ AOC2404/Program.cs
@@ -0,0 +1,98 @@
+﻿
+var path = Path.Combine("..", "..", "..", "..", "input04.txt");
+var input = File.ReadAllLines(path);
+
+int xmasCount = Problem1(input);
+int masCount = Problem2(input);
+
+Console.WriteLine("Problem 1: " + xmasCount);
+Console.WriteLine("Probelem 2: " + masCount);
+
+static int Problem1(string[] input)
+{
+    string word = "XMAS";
+    int count = 0;
+    for (int row = 0; row < input.Length; row++)
+    {
+        for (int col = 0; col < input[row].Length; col++)
+        {
+            // Horisontellt framåt
+            if (CheckDirection(input, row, col, 0, 1, word))
+                count++;
+            // Horisontellt bakåt
+            if (CheckDirection(input, row, col, 0, -1, word))
+                count++;
+            // Vertikalt nedåt
+            if (CheckDirection(input, row, col, 1, 0, word))
+                count++;
+            // Vertikalt uppåt
+            if (CheckDirection(input, row, col, -1, 0, word))
+                count++;
+            // Diagonalt nedåt höger
+            if (CheckDirection(input, row, col, 1, 1, word))
+                count++;
+            // Diagonalt uppåt höger
+            if (CheckDirection(input, row, col, -1, 1, word))
+                count++;
+            // Diagonalt nedåt vänster
+            if (CheckDirection(input, row, col, 1, -1, word))
+                count++;
+            // Diagonalt uppåt vänster
+            if (CheckDirection(input, row, col, -1, -1, word))
+                count++;
+        }
+    }
+    return count;
+}
+
+static int Problem2(string[] input)
+{
+    int count = 0;
+    for (int row = 1; row < input.Length - 1; row++)
+    {
+        for (int col = 1; col < input[row].Length - 1; col++)
+        {
+            if (input[row][col] == 'A')
+            {
+                if (isXmas(input, row, col))
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+static bool CheckDirection(string[] input, int row, int col, int rowDirection, int colDirection, string word)
+{
+    for (int i = 0; i < word.Length; i++)
+    {
+        int newRow = row + i * rowDirection;
+        int newCol = col + i * colDirection;
+
+        if (newRow < 0 || newRow >= input.Length || newCol < 0 || newCol >= input.Length)
+            return false;
+
+        if (input[newRow][newCol] != word[i])
+            return false;
+    }
+    return true;
+}
+
+static bool isXmas(string[] input, int row, int col)
+{
+    char[] mas = new char[] { 'M', 'A', 'S' };
+    char[] sam = new char[] { 'S', 'A', 'M' };
+
+    char[] diagonalUpDown = new char[] { input[row + 1][col - 1], input[row][col], input[row - 1][col + 1] };
+    char[] diagonalDownUp = new char[] { input[row - 1][col - 1], input[row][col], input[row + 1][col + 1] };
+
+    if ((diagonalUpDown.SequenceEqual(mas) || diagonalUpDown.SequenceEqual(sam)) &&
+        (diagonalDownUp.SequenceEqual(mas) || diagonalDownUp.SequenceEqual(sam)))
+    {
+        return true;
+    }
+
+    return false;
+}
