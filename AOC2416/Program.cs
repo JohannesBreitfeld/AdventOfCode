e1282b0f2023a1eaaba21ebb4f453f56dab6e241
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-12-28 13:23:56 +0100

    Solved part two for Day16. Added new project for day 17, solved part one.

--- AOC2416/Program.cs
+++ AOC2416/Program.cs
@@ -2,25 +2,52 @@
 var PartOne = new PartOne();
 
 PartOne.ReadMap();
-Console.WriteLine(PartOne.CalculateScore());
+
+Console.WriteLine(PartOne.FindBestPath());
+//var map = PartOne.map;
+//var bestPath = PartOne.bestPaths;
+
+
+//for (int i = 0; i < map.GetLength(0); i++)
+//{
+//    for (int j = 0; j < map.GetLength(1); j++)
+//    {
+//       Console.Write(map[i, j]);
+//    }
+//    Console.WriteLine();
+//}
+//foreach (var item in bestPath)
+//{
+//    Console.WriteLine(item.Count);
+//}
+//Console.ForegroundColor = ConsoleColor.Blue;
+//foreach (var path in bestPath)
+//{
+//    foreach (var coordinate in path)
+//    {
+//        Console.SetCursorPosition(coordinate.x, coordinate.y);
+//        Console.Write('O');
+//    }
+//}
+//Console.ResetColor();
 
 
 class PartOne
 {
     private int rows;
     private int cols;
-    private char[,]? map;
+    public char[,]? map;
     private int startX;
     private int startY;
     private int endX;
     private int endY;
-    private int[] dx = { 0, 1, 0, -1 }; 
-    private int[] dy = { -1, 0, 1, 0 }; 
-    //private char[] directions = { 'N', 'E', 'S', 'W' };
-
+    private int[] dx = { 0, 1, 0, -1 };
+    private int[] dy = { -1, 0, 1, 0 };
+    public HashSet<(int, int)> visited = new();
+ 
     public void ReadMap()
     {
-        var path = Path.Combine("..", "..", "..", "..", "input16.txt");
+        var path = Path.Combine("..", "..", "..", "..", "test16.txt");
         var inputMap = File.ReadAllLines(path);
 
         rows = inputMap.Length;
@@ -44,12 +71,9 @@ class PartOne
                     endY = i;
                 }
             }
-        } 
+        }
     }
 
-    // Läs på mer om Djikstras algortihm och se över denna funktionen igen.
-    // Blir nog fel på första rörelsen eftersom det alltid diffar +/-1000.
-    
     public int CalculateScore()
     {
         var pq = new PriorityQueue<(int x, int y, int dir), int>();
@@ -62,7 +86,7 @@ class PartOne
                     score[(i, j, d)] = int.MaxValue;
 
         score[(startX, startY, 1)] = 0;
-        pq.Enqueue((startX, startY, 1), 0); 
+        pq.Enqueue((startX, startY, 1), 0);
 
         while (pq.Count > 0)
         {
@@ -72,37 +96,134 @@ class PartOne
             if (x == endX && y == endY)
             {
                 lowestScore = Math.Min(lowestScore, currentScore);
+                break;
             }
 
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+            int newX = x + dx[dir];
+            int newY = y + dy[dir];
 
-            if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && map![nx, ny] != '#')
+            if (map[newY, newX] != '#')
             {
-                if (currentScore + 1 < score[(nx, ny, dir)])
+                if (currentScore + 1 < score[(newX, newY, dir)])
                 {
-                    score[(nx, ny, dir)] = currentScore + 1;
-                    pq.Enqueue((nx, ny, dir), score[(nx, ny, dir)]);
+                    score[(newX, newY, dir)] = currentScore + 1;
+                    pq.Enqueue((newX, newY, dir), score[(newX, newY, dir)]);
                 }
             }
 
             int newDirClockwise = (dir + 1) % 4;
-            if (currentScore + 1000 < score[(x, y, newDirClockwise)]) 
+            if (currentScore + 1000 < score[(x, y, newDirClockwise)])
             {
                 score[(x, y, newDirClockwise)] = currentScore + 1000;
                 pq.Enqueue((x, y, newDirClockwise), score[(x, y, newDirClockwise)]);
             }
 
             int newDirCounterClockwise = (dir + 3) % 4;
-            if (currentScore + 1000 < score[(x, y, newDirCounterClockwise)]) 
+            if (currentScore + 1000 < score[(x, y, newDirCounterClockwise)])
             {
                 score[(x, y, newDirCounterClockwise)] = currentScore + 1000;
                 pq.Enqueue((x, y, newDirCounterClockwise), score[(x, y, newDirCounterClockwise)]);
             }
+
         }
         return lowestScore;
     }
+
+    public int FindBestPath()
+    {
+        var pq = new PriorityQueue<(int x, int y, int dir), int>();
+        var minimumCost = new Dictionary<(int x, int y, int dir), int>();
+        var finalized = new HashSet<(int x, int y, int dir)>();
+        var previous = new Dictionary<(int x, int y, int dir), HashSet<(int x, int y, int dir)>>();
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                for (int d = 0; d < 4; d++)
+                    minimumCost[(i, j, d)] = int.MaxValue;
+
+        minimumCost[(startX, startY, 1)] = 0;
+        pq.Enqueue((startX, startY, 1), 0);
+
+        while (pq.Count > 0)
+        {
+            var (x, y, dir) = pq.Dequeue();
+            finalized.Add((x, y, dir));
+
+            int newX = x + dx[dir];
+            int newY = y + dy[dir];
+
+            var nextState = (newX, newY, dir);
+            AddNextIfImproved(nextState, minimumCost[(x, y, dir)] + 1, (x, y, dir));
+
+            int newDirClockwise = (dir + 1) % 4;
+            nextState = (x, y, newDirClockwise);
+            AddNextIfImproved(nextState, minimumCost[(x, y, dir)] + 1000, (x, y, dir));
+
+            int newDirCounterClockwise = (dir + 3) % 4;
+            nextState = (x, y, newDirCounterClockwise);
+            AddNextIfImproved(nextState, minimumCost[(x, y, dir)] + 1000, (x, y, dir));
+
+        }
+        var bestEndState = int.MaxValue;
+
+        for (int i = 0; i < 4; i++)
+        {
+            bestEndState = Math.Min(bestEndState, minimumCost.GetValueOrDefault((endX, endY, i)));
+        }
+        Console.WriteLine(bestEndState);
+
+        var bestPaths = new HashSet<(int x, int y)>();
+        Queue<(int x, int y, int dir)> backTrackQueue = new();
+        
+        for (int i = 0; i < 4; i++)
+        {
+            if (minimumCost.TryGetValue((endX, endY, i), out int value) && value == bestEndState)
+            {
+                backTrackQueue.Enqueue((endX, endY, i));
+            }
+        }
+
+        while (backTrackQueue.Count > 0)
+        {
+            var current = backTrackQueue.Dequeue();
+            bestPaths.Add((current.x, current.y));
+            
+            if (previous.TryGetValue(current, out var sources))
+            {
+                foreach (var source in sources)
+                {
+                    backTrackQueue.Enqueue(source);
+                }
+            }
+        }
+
+        return bestPaths.Count();
+
+
+        void AddNextIfImproved((int x, int y, int dir) nextState, int cost, (int x, int y, int dir) state)
+        {
+            if (map[nextState.y, nextState.x] == '#')
+            {
+                return;
+            }
+            //if (!finalized.Contains(nextState))
+            //{
+                if (!minimumCost.TryGetValue(nextState, out var nextCost) || nextCost > minimumCost[state])
+                {
+                    minimumCost[nextState] = cost;
+                    previous[nextState] = new();
+                    pq.Enqueue(nextState, minimumCost[nextState]);
+                }
+                if (minimumCost[nextState] == cost)
+                {
+                    previous[nextState].Add(state);
+                }
+            //}
+        }
+
+    }
 }
+
 
 
 
---
+++ AOC2417/Program.cs
@@ -0,0 +1,85 @@
+﻿
+var partOne = new AOC2417();
+partOne.Excecute();
+
+public class AOC2417
+{
+    private int regA = 33024962;
+    private int regB = 0;
+    private int regC = 0;
+
+    private int[] program = "2,4,1,3,7,5,1,5,0,3,4,2,5,5,3,0"
+        .Split(',')
+        .Select(n => int.Parse(n))
+        .ToArray();
+
+    private int pointer = 0;
+    public List<int> output = new List<int>();
+
+    public void Excecute()
+    {
+        while (pointer < program.Length)
+        {
+            var instruction = program[pointer];
+            var operand = program[pointer + 1];
+
+            Operation(instruction, operand);
+            pointer += 2;
+        }
+
+        for (int i = 0; i < output.Count; i++)
+        {
+            Console.Write(output[i]);
+            if (i != output.Count - 1)
+            {
+                Console.Write(',');
+            }
+        }
+    }
+
+    private int Combo(int operand)
+    {
+        if (operand >= 0 && operand <= 3) return operand;
+        else if (operand == 4) return regA;
+        else if (operand == 5) return regB;
+        else if (operand == 6) return regC;
+        throw new ArgumentException($"{operand}");
+    }
+
+    private void Operation(int instruction, int operand)
+    {
+        switch (instruction)
+        {
+            case 0:
+                regA = regA >> Combo(operand);
+                break;
+            case 1:
+                regB = regB ^ operand;
+                break;
+            case 2:
+                regB = Combo(operand) % 8;
+                break;
+            case 3:
+                if (regA != 0)
+                {
+                    pointer = operand - 2;
+                }
+                break;
+            case 4:
+                regB = regB ^ regC;
+                break;
+            case 5:
+                output.Add(Combo(operand) % 8);
+                break;
+            case 6:
+                regB = regA >> Combo(operand);
+                break;
+            case 7:
+                regC = regA >> Combo(operand);
+                break;
+            default:
+                throw new ArgumentException($"{instruction}");
+
+        }
+    }
+}
